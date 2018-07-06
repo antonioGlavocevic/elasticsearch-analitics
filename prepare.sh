77524c466c6a7fc5590b83a820a6e19e256bc501
Antonio Glavocevic <tony@it-tony-mac.local>
2018-07-06 15:11:38 +1000

    chmod go-w

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo chown 1000:root $DIR/packetbeat/packetbeat.yml
+sudo chmod go-w $DIR/packetbeat/packetbeat.yml
 sudo chown 1000:root $DIR/logstash/log
 sudo chown -R 1000:root $DIR/elasticsearch/data
--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 \
-  -v $PWD/elasticsearch/data:/usr/share/elasticsearch/data \
-  -v $PWD/elasticsearch/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+  -v $DIR/elasticsearch/data:/usr/share/elasticsearch/data \
+  -v $DIR/elasticsearch/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
   -e "discovery.type=single-node" \
   docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
 
 docker run -d --name logstash -p 9600:9600 -p 5044:5044 \
-  -v $PWD/logstash/pipeline:/usr/share/logstash/pipeline \
-  -v $PWD/logstash/log:/usr/share/logstash/log \
+  -v $DIR/logstash/pipeline:/usr/share/logstash/pipeline \
+  -v $DIR/logstash/log:/usr/share/logstash/log \
   docker.elastic.co/logstash/logstash-oss:6.2.4
 
 docker run -d --name packetbeat --network host --cap-add=NET_ADMIN \
-  -v $PWD/packetbeat/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml \
+  -v $DIR/packetbeat/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml \
   docker.elastic.co/beats/packetbeat:6.2.4
