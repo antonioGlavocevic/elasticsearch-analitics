08ee1c60d1a694107fe6815a230250ef45ad5a9e
Antonio Glavocevic <tony@it-tony-mac.local>
2018-07-06 14:47:00 +1000

    relative dir path

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo chown root:root $PWD/packetbeat/packetbeat.yml
-sudo chown 1000:root $PWD/logstash/log
-sudo chown -R 1000:root $PWD/elasticsearch/data
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sudo chown root:root $DIR/packetbeat/packetbeat.yml
+sudo chown 1000:root $DIR/logstash/log
+sudo chown -R 1000:root $DIR/elasticsearch/data
