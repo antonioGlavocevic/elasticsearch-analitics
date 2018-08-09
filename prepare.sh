82c09bd0b7927bf03cf59152c79cafa6a27c022a
Antonio Glavocevic <tony@it-tony-mac.local>
2018-08-09 05:54:51 +0000

    prepare chown change

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo chown 1000:root $DIR/packetbeat/packetbeat.yml
+sudo chown 1000 $DIR/packetbeat/packetbeat.yml
 sudo chmod go-w $DIR/packetbeat/packetbeat.yml
-sudo chown 1000:root $DIR/logstash/log
-sudo chown -R 1000:root $DIR/elasticsearch/data
+sudo chown 1000 $DIR/logstash/log
+sudo chown -R 1000 $DIR/elasticsearch/data
