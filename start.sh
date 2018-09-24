6225a864a86993dd6e5aa2f8d51eb74ba3e3a0e0
Antonio Glavocevic <tony@it-tony-mac.local>
2018-09-24 14:46:59 +1000

    skip logstash

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ docker run -d --name elasticsearch -p 127.0.0.1:9200:9200 -p 127.0.0.1:9300:9300
   -e "discovery.type=single-node" \
   docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
 
-docker run -d --name logstash -p 127.0.0.1:9600:9600 -p 127.0.0.1:5044:5044 \
-  -v $DIR/logstash/pipeline:/usr/share/logstash/pipeline \
-  -v $DIR/logstash/config/logstash.yml:/usr/share/logstash/config/logstash.yml \
-  -v $DIR/logstash/log:/usr/share/logstash/log \
-  docker.elastic.co/logstash/logstash-oss:6.2.4
+# docker run -d --name logstash -p 127.0.0.1:9600:9600 -p 127.0.0.1:5044:5044 \
+#   -v $DIR/logstash/pipeline:/usr/share/logstash/pipeline \
+#   -v $DIR/logstash/config/logstash.yml:/usr/share/logstash/config/logstash.yml \
+#   -v $DIR/logstash/log:/usr/share/logstash/log \
+#   docker.elastic.co/logstash/logstash-oss:6.2.4
 
 docker run -d --name packetbeat --network host --cap-add=NET_ADMIN \
   -v $DIR/packetbeat/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml \
